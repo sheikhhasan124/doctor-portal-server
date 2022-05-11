7b53c8e99898f9b4516fb6713a864d6fd8849cfb
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-11 16:08:54 +0600

    basic server create

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello Home')
